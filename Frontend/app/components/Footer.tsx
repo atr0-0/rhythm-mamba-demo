e4e116a65b87178c1e451077fc293a7a849b25ed
atr0-0 <shubham.tandon2023@gmail.com>
2026-01-26 15:04:39 +0530

    docs: clarify local run paths

--- Frontend/app/components/Footer.tsx
+++ Frontend/app/components/Footer.tsx
@@ -13,18 +13,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/X';
 import SitemarkIcon from './SitemarkIcon';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" sx={{ color: 'text.secondary', mt: 1 }}>
-      {'Copyright © '}
-      <Link color="text.secondary" href="https://mui.com/">
-        Sitemark
-      </Link>
-      &nbsp;
-      {new Date().getFullYear()}
-    </Typography>
-  );
-}
+
 
 export default function Footer() {
   return (
